f8dbb702e84fbc650fa66a752626317b2564fd9f
ArturHarutyunyan1 <arturhar@outlook.com>
2024-01-16 18:57:28 +0400

    ft_memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	char		*d;
+	const char	*s;
+	size_t		i;
+
+	i = 0;
+	d = (char *)dest;
+	s = (const char *)src;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+}
