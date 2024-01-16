987ace758f634317265e97b27352e374cdfdcc5b
ArturHarutyunyan1 <arturhar@outlook.com>
2024-01-16 19:49:12 +0400

    ft_toupper

--- libft.h
+++ libft.h
@@ -16,6 +16,7 @@ int     ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
+int	ft_toupper(int c);
 
 size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dest, const char *src, size_t n);
