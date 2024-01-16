9c5eaef2a52b3e199d613af198da9d9b07421ab6
ArturHarutyunyan1 <arturhar@outlook.com>
2024-01-16 19:12:40 +0400

    memset

--- ft_memset.c
+++ ft_memset.c
@@ -1,6 +1,6 @@
 #include "libft.h"
 
-void	*ft_memset(void *ptr, int value, size_t	num)
+void	*ft_memset(void *ptr, int value, size_t	n)
 {
 	char 		*str;
 	size_t		i;
@@ -9,7 +9,7 @@ void	*ft_memset(void *ptr, int value, size_t	num)
 	i = 0;
 	str = (char *)ptr;
 	val = (unsigned char)value;
-	while (i < num)
+	while (i < n)
 		str[i++] = val;
 	return (str);
 }
--- libft.h
+++ libft.h
@@ -5,7 +5,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
-#include <string.h>
+
+void	*ft_memset(void *ptr, int value, size_t	n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	ft_bzero(void *s, size_t n);
 
 int	ft_isalpha(int c);
 int     ft_isdigit(int c);
@@ -14,4 +18,5 @@ int	ft_isascii(int c);
 int	ft_isprint(int c);
 
 size_t	ft_strlen(const char *str);
+size_t	ft_strlcpy(char *dest, const char *src, size_t n);
 #endif
