48ec0c3d0caf95762ce1b0d94762a1c8617b84da
ArturHarutyunyan1 <arturhar@outlook.com>
2024-01-16 19:09:37 +0400

    ft_memmove

---
+++ ft_memmove.c
@@ -0,0 +1,26 @@
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	char		*d;
+	const char 	*s;
+	size_t		i;
+
+	i = 0;
+	d = (char *)dest;
+	s = (const char *)src;
+	if (d > s)
+	{
+		while (n-- > 0)
+			d[n] = s[n];
+	}
+	else
+	{
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	return (d);
+}
