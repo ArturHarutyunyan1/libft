e14776e8ee7331030785e1ec1fe1b5f13b629e93
ArturHarutyunyan1 <arturhar@outlook.com>
2024-01-17 17:27:54 +0400

    ft_striteri

--- libft.h
+++ libft.h
@@ -24,6 +24,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	ft_bzero(void *s, size_t n);
 void    *ft_calloc(size_t num, size_t size);
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 
 char	*ft_strchr(const char *str, int c);
 char	*ft_strrchr(const char *str, int c);
