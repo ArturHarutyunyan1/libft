4e9cc6920e61f65fc979338e6633182b2d94cacc
Artur Harutyunyan <arturhar@c2r4s1.42yerevan.am>
2024-01-22 11:48:39 +0400

    libft

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,22 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arturhar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/20 17:31:50 by arturhar          #+#    #+#             */
+/*   Updated: 2024/01/20 19:49:13 by arturhar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "libft.h"
+#define UNIT_MAX_SQRT 65535
 
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
-	size_t	i;
-	i = 0;
-	if (count != 0 && (count * size) / count != size)
+
+	if ((count > UNIT_MAX_SQRT || size > UNIT_MAX_SQRT) && (count > 0 && UNIT_MAX_SQRT / count < size))
 		return (NULL);
-	ptr = (void *)malloc(size * count);
+	ptr = malloc(count * size);
 	if (!ptr)
 		return (NULL);
-	if (size == 0)
-		return (ptr);
-	i = 0;
-	while (i < (count * size))
-	{
-		ft_memset(ptr, 0, count * size);
-		i++;
-	}
+	ft_bzero (ptr, count * size);
 	return (ptr);
+
 }
