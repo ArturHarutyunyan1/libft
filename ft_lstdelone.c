cbd86c054e8eaac0c89e2cbf42a0c60b32d4866f
Artur Harutyunyan <arturhar@c2r16s3.42yerevan.am>
2024-01-26 17:18:45 +0400

    vaspur

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,14 +6,14 @@
 /*   By: arturhar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 16:54:06 by arturhar          #+#    #+#             */
-/*   Updated: 2024/01/25 16:54:38 by arturhar         ###   ########.fr       */
+/*   Updated: 2024/01/26 17:16:24 by arturhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-    (del)(lst->content);
-    free(lst);
-    lst = NULL;
+	(del)(lst->content);
+	free(lst);
+	lst = NULL;
 }
--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,26 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arturhar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/26 17:17:04 by arturhar          #+#    #+#             */
+/*   Updated: 2024/01/26 17:18:26 by arturhar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-    t_list *result;
-    t_list *node;
-    t_list *val;
+	t_list	*result;
+	t_list	*node;
+	t_list	*val;
 
-    if (!lst || !f || !del)
-        return (NULL);
-    result = NULL;
-    while (lst != NULL)
-    {
-        val = f(lst->content);
-        node = ft_lstnew(val);
-        if (!node)
-        {
-            del(val);
-            ft_lstclear(&result, (*del));
-            return (result);
-        }
-        ft_lstadd_back(&result, node);
-        lst = lst->next;
-    }
-    return (result);
+	if (!lst || !f || !del)
+		return (NULL);
+	result = NULL;
+	while (lst != NULL)
+	{
+		val = f(lst->content);
+		node = ft_lstnew(val);
+		if (!node)
+		{
+			del(val);
+			ft_lstclear(&result, (*del));
+			return (result);
+		}
+		ft_lstadd_back(&result, node);
+		lst = lst->next;
+	}
+	return (result);
 }
